c1dadcc5b94e85260bd0bfa23a98c2524814ed8d
Andy Shen <superkangkangkang@gmail.com>
2024-11-13 12:13:03 +0800

    UserCards UI

--- School-starter/src/app/[locale]/(dashboard)/admin/page.tsx
+++ School-starter/src/app/[locale]/(dashboard)/admin/page.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import UserCard from '../../components/UserCard'
 
 const AdminPage = () => {
   return (
-    <div>AdminPage</div>
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/* left */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+      {/* right */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8"></div>
+    </div>
   )
 }
 
